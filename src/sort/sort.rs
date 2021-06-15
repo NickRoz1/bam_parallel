d306449340c9131e7e703c02c5126b5829006a80
nickr <nick.rozinsky@gmail.com>
2021-06-15 19:26:42 +0300

    Continue working on parallel sort.

--- src/sort/sort.rs
+++ src/sort/sort.rs
@@ -1,10 +1,12 @@
-use crate::ParallelReader;
+use crate::{ParallelReader, MEGA_BYTE_SIZE};
 // use crate::{GIGA_BYTE_SIZE, MEGA_BYTE_SIZE};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use flume::Sender;
 use gbam_tools::BAMRawRecord;
 use lz4_flex;
 use rayon::prelude::ParallelSliceMut;
 use rayon::{self, ThreadPoolBuilder};
+use reorder::reorder_index;
 
 use super::comparators::{
     compare_coordinates_and_strand, compare_read_names, compare_read_names_and_mates,
@@ -14,41 +16,51 @@ use std::cell::RefCell;
 use std::cmp::{max, min, Ordering};
 use std::fs::File;
 use std::io::{BufWriter, Read, Write};
+use std::mem;
+use std::ops::Range;
 use std::path::PathBuf;
 use tempdir::TempDir;
 
 /// This struct manages buffer for unsorted reads
 // #[derive(Send)]
-struct RecordsBuffer<'a> {
+struct RecordsBuffer {
     mem_limit: usize,
-    records_bytes: RefCell<Vec<u8>>,
+    records_bytes: Vec<u8>,
+    // Could be done this way with some unsafe code - because self referential
+    // struct:
+    // ---
     // This will hold "pointers" to actual data in buffers (to avoid
     // allocations). Since BAMRawRecord is a wrapper struct, this will help
     // access records data when sorting. If owned BAMRawRecord would be used (so
     // there is no need in records_bytes field), in case when there is record
     // bigger than previous the allocation will happen, in borrowed case just
     // more space will be occupied in the buffer and less records will fit in.
-    records: Vec<BAMRawRecord<'a>>,
+    // records: Vec<BAMRawRecord>,
+    // ---
+    // This will hold ranges corresponding to byte spans for each record in
+    // records_bytes. When we'll need to operate on record fields, record wrappers will
+    // be constructed on the fly.
+    records: Vec<Range<usize>>,
 }
 
-impl<'a> RecordsBuffer<'a> {
+impl RecordsBuffer {
     pub fn new(mem_limit: usize) -> Self {
         RecordsBuffer {
             mem_limit,
-            records_bytes: RefCell::new(vec![0; mem_limit]),
+            records_bytes: vec![0; mem_limit],
             records: Vec::new(),
         }
     }
 
     fn clear(&mut self) {
         self.records.clear();
-        self.records_bytes.borrow_mut().clear();
+        self.records_bytes.clear();
     }
 
-    pub fn fill<R>(&'a mut self, reader: &mut ParallelReader<R>) {
+    pub fn fill<R>(&mut self, reader: &mut ParallelReader<R>) {
         self.clear();
         let mut last_byte_offset: usize = 0;
-        let mut buf = &mut self.records_bytes.borrow_mut()[..];
+        let mut buf = &mut self.records_bytes[..];
         loop {
             let block_size = reader.read_block_size().unwrap();
             // We have to read at least something in the buffer, hence '> 0' check.
@@ -59,13 +71,9 @@ impl<'a> RecordsBuffer<'a> {
             reader
                 .read_exact(&mut buf[last_byte_offset..last_byte_offset + block_size])
                 .unwrap();
-            // This is done to convince borrow checker that we do not borrow
-            // same part of slice twice.
-            let (loaded_data, rest_of_the_buffer) = buf.split_at_mut(last_byte_offset + block_size);
-            // We don't need first part of the buffer anymore, since it's already filled.
-            buf = rest_of_the_buffer;
+            // Push the range of bytes which this record occupies
             self.records
-                .push(BAMRawRecord(std::borrow::Cow::Borrowed(loaded_data)));
+                .push(last_byte_offset..last_byte_offset + block_size);
             last_byte_offset += block_size;
         }
     }
@@ -95,7 +103,7 @@ pub fn sort_bam<R: Read + Send + 'static, W>(
 
     let mut parallel_reader = ParallelReader::new(reader, reader_thread_num);
 
-    let tmp_file_num = read_split_sort_dump_chunks(
+    let tmp_file_names = read_split_sort_dump_chunks(
         &mut parallel_reader,
         mem_limit,
         decompress_thread_num,
@@ -112,9 +120,9 @@ fn read_split_sort_dump_chunks<R: Read + Send + 'static>(
     tmp_dir_path: PathBuf,
     compress_temp_files: bool,
     sort_by: SortBy,
-) -> usize {
+) -> Vec<File> {
     let (buf_send, buf_recv) = flume::unbounded();
-    let mut recs_buf = Some(RecordsBuffer::<'static>::new(mem_limit / 2));
+    let mut recs_buf = Some(RecordsBuffer::new(mem_limit / 2));
 
     buf_send.send(RecordsBuffer::new(mem_limit / 2)).unwrap();
     let tmp_dir = TempDir::new_in(tmp_dir_path, "BAM sort temporary directory.").unwrap();
@@ -124,7 +132,8 @@ fn read_split_sort_dump_chunks<R: Read + Send + 'static>(
         .build()
         .unwrap();
 
-    let mut temp_files_counter: usize = 0;
+    let mut temp_files = Vec::<File>::new();
+    let mut temp_files_counter = 0;
 
     // Load first chunk to start the cycle.
     if !reader.empty() {
@@ -136,17 +145,20 @@ fn read_split_sort_dump_chunks<R: Read + Send + 'static>(
         recs_buf = Some(RecordsBuffer::new(mem_limit / 2));
     } else {
         // Empty file
-        return 0;
+        return Vec::new();
     }
     while !reader.empty() {
-        // recs_buf.as_mut().unwrap().clear();
-        // recs_buf.as_mut().unwrap().fill(reader);
+        recs_buf.as_mut().unwrap().clear();
+        recs_buf.as_mut().unwrap().fill(reader);
         let taken_buf = recs_buf.take().unwrap();
         let send_channel = buf_send.clone();
         thread_pool.spawn(move || sort_chunk(taken_buf, send_channel, sort_by));
         recs_buf = Some(buf_recv.recv().unwrap());
 
-        let temp_file = make_tmp_file(&temp_files_counter.to_string(), &tmp_dir).unwrap();
+        let file_name = temp_files_counter.to_string();
+        let temp_file = make_tmp_file(&file_name, &tmp_dir).unwrap();
+        temp_files.push(temp_file);
+
         dump(recs_buf.as_ref().unwrap(), temp_file, compress_temp_files).unwrap();
         temp_files_counter += 1;
     }
@@ -155,11 +167,17 @@ fn read_split_sort_dump_chunks<R: Read + Send + 'static>(
     // One can optimize this so when there is no temp files written (there is
     // only one chunk in file) it's written straight to the resulting file, but
     // it's probably not worth it.
+    let file_name = temp_files_counter.to_string();
     let temp_file = make_tmp_file(&temp_files_counter.to_string(), &tmp_dir).unwrap();
     dump(&last_buf, temp_file, compress_temp_files).unwrap();
-    temp_files_counter += 1;
+    temp_files.push(temp_file);
 
-    return temp_files_counter;
+    // Ensure all buffered data is written
+    temp_files
+        .iter_mut()
+        .for_each(|file| file.sync_all().unwrap());
+
+    return temp_files;
 }
 
 fn dump(buf: &RecordsBuffer, file: File, compress_temp_files: bool) -> std::io::Result<()> {
@@ -176,29 +194,79 @@ fn dump(buf: &RecordsBuffer, file: File, compress_temp_files: bool) -> std::io::
 
 fn write<W: Write>(buf: &RecordsBuffer, writer: &mut W) -> std::io::Result<()> {
     for rec in &buf.records {
-        writer.write_all(&rec.0)?;
+        let rec_size = (rec.end - rec.start) as u64;
+        writer.write_u64::<LittleEndian>(rec_size)?;
+        writer.write_all(&buf.records_bytes[rec.start..rec.end])?;
     }
     Ok(())
 }
 
-fn sort_chunk<'a>(
-    mut buf: RecordsBuffer<'a>,
-    buf_return: Sender<RecordsBuffer<'a>>,
-    sort_by: SortBy,
-) {
-    &buf.records[..].par_sort_by(get_comparator(sort_by));
+// For each byte range construct record wrapper, sort records and then reorder
+// the ranges to then write byte ranges into file in sorted order.
+fn sort_chunk(buf: RecordsBuffer, buf_return: Sender<RecordsBuffer>, sort_by: SortBy) {
+    let mut record_wrappers: Vec<(BAMRawRecord, Range<usize>)> = buf
+        .records
+        .into_iter()
+        .map(|range| {
+            (
+                BAMRawRecord(std::borrow::Cow::Borrowed(
+                    &buf.records_bytes[range.start..range.end],
+                )),
+                range,
+            )
+        })
+        .collect();
+    &record_wrappers[..].par_sort_by(get_comparator(sort_by));
+    // The BAMRawRecords and their corresponding ranges are now in
+    // order. Replace original ranges with the sorted ones.
+    buf.records = record_wrappers.into_iter().map(|rec| rec.1).collect();
     buf_return.send(buf).unwrap();
 }
 
-fn get_comparator(sort_by: SortBy) -> fn(&BAMRawRecord, &BAMRawRecord) -> Ordering {
-    match sort_by {
+fn get_comparator(
+    sort_by: SortBy,
+) -> impl Fn(&(BAMRawRecord, Range<usize>), &(BAMRawRecord, Range<usize>)) -> Ordering {
+    let cmp = match sort_by {
         SortBy::Name => compare_read_names,
         SortBy::NameAndMatchMates => compare_read_names_and_mates,
         SortBy::CoordinatesAndStrand => compare_coordinates_and_strand,
-    }
+    };
+    move |a, b| cmp(&a.0, &b.0)
 }
 
 fn make_tmp_file(file_name: &str, tmp_dir: &TempDir) -> std::io::Result<std::fs::File> {
     let file_path = tmp_dir.path().join(file_name);
     File::create(file_path)
+}
+
+// Struct which manages reading chunks from files
+struct ChunkReader<R>
+where
+    R: Read,
+{
+    inner: R,
+    cur_rec: Vec<u8>,
+}
+
+impl<R> ChunkReader<R> {
+    // Returns how many bytes were read
+    pub fn next_rec(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        // Needed to check whether EOF is reached.
+        let len_buf: [u8; 8] = [0; 8];
+        if self.inner.read(len_buf)? == 0 {
+            // EOF reached.
+            return Ok(0);
+        }
+        let data_len = len_buf.read_u64::<LittleEndian>()?;
+    }
+}
+
+fn merge_sorted_chunks_and_write<W: Write>(
+    mem_limit: usize,
+    tmp_files: Vec<File>,
+    sort_by: SortBy,
+    writer: &mut W,
+) -> std::io::Result<()> {
+    let num_chunks = tmp_files.len();
+    let input_buf_mem_limit = min(16 * MEGA_BYTE_SIZE, mem_limit / 4 / num_chunks);
 }
